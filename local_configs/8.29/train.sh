72db9ecc6e6b6dea78cf97e767d96ac4ade6208b
Your Name <you@example.com>
2021-08-30 10:17:01 +0800

    lp=0

--- local_configs/8.29/train.sh
+++ local_configs/8.29/train.sh
@@ -1,8 +1,8 @@
-bash tools/dist_train.sh local_configs/8.29/1.py 2;
+bash tools/dist_train.sh local_configs/8.29/1.py 8;
 sleep 10s;
-bash tools/dist_train.sh local_configs/8.29/distill.py 2;
+bash tools/dist_train.sh local_configs/8.29/distill.py 8;
 sleep 10s;
-bash tools/dist_train.sh local_configs/8.29/2.py 2;
+bash tools/dist_train.sh local_configs/8.29/2.py 8;
 sleep 10s;
-bash tools/dist_train.sh local_configs/8.29/distill_T=2.py 2;
+bash tools/dist_train.sh local_configs/8.29/distill_T=2.py 8;
 sleep 10s;
