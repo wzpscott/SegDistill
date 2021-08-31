934d3c8323c267dc001197a8f6db770aca3fa492
Your Name <you@example.com>
2021-08-31 11:01:28 +0800

    commit

--- local_configs/8.29/train.sh
+++ local_configs/8.29/train.sh
@@ -1,7 +1,5 @@
 bash tools/dist_train.sh local_configs/8.29/1.py 8;
 sleep 10s;
-bash tools/dist_train.sh local_configs/8.29/distill.py 8;
-sleep 10s;
 bash tools/dist_train.sh local_configs/8.29/2.py 8;
 sleep 10s;
 bash tools/dist_train.sh local_configs/8.29/distill_T=2.py 8;
