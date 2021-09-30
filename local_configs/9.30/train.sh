06b360f87b394b54d62bfa308e1d33fb1c439a8b
Your Name <you@example.com>
2021-10-01 00:15:45 +0800

    10.1

--- local_configs/9.30/train.sh
+++ local_configs/9.30/train.sh
@@ -1,6 +1,14 @@
 bash tools/dist_train.sh local_configs/9.30/attention_mse.py 8;
 sleep 10;
+bash tools/dist_train.sh local_configs/9.30/attention_s.py 8;
+sleep 10;
+bash tools/dist_train.sh local_configs/9.30/attention_c.py 8;
+sleep 10;
 bash tools/dist_train.sh local_configs/9.30/feature_mse.py 8;
+sleep 10;
+bash tools/dist_train.sh local_configs/9.30/feature_s.py 8;
+sleep 10;
+bash tools/dist_train.sh local_configs/9.30/feature_c.py 8;
 sleep 10;
 bash tools/dist_train.sh local_configs/9.30/logits_c_mask.py 8;
 sleep 10;
