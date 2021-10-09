a252f8a4fee8087123028c1fe5f186fa13641bce
Your Name <you@example.com>
2021-10-09 21:57:36 +0800

    rrr

--- mmseg/models/distillation/losses.py
+++ mmseg/models/distillation/losses.py
@@ -5,177 +5,32 @@ import torch
 import numpy as np
 from torch.nn import functional as F
 from mmseg.ops import resize
-        
-# class Baseloss(nn.Module):
-#     '''
-#     Other losses can inherit Baseloss class and rewrite the tranform function to get desirable output shape.
-#     When rewriting, permute the output to put the dimension you want to apply softmax to the last 
-#         because by default all loss function apply softmax to the last dimention followed by a KLDivLoss
-#     '''
-#     def __init__(self,weight,tau,softmax_dim=-1):
-#         super().__init__()
-#         self.weight = weight
-#         self.tau = tau
-#         self.softmax_dim = softmax_dim
-#         self.KLDiv = torch.nn.KLDivLoss(reduction='sum')
-#     def resize(self,x,gt_semantic_seg,mode='bilinear'):
-#             x= resize(
-#                 input=x,
-#                 size=gt_semantic_seg.shape[2:],
-#                 mode=mode,
-#                 align_corners=False)
-#             return x
-#     def transform(self,x,gt_semantic_seg):
-#         return x
-#     def forward(self,x_student,x_teacher,gt_semantic_seg):
-#         x_student,x_teacher = self.transform(x_student,gt_semantic_seg),self.transform(x_teacher,gt_semantic_seg)
-#         x_student = F.log_softmax(x_student/self.tau,dim=self.softmax_dim)
-#         x_teacher = F.softmax(x_teacher/self.tau,dim=self.softmax_dim)
-#         loss = self.weight*self.KLDiv(x_student,x_teacher)
-#         return loss/(x_student.numel()/x_student.shape[-1])
 
-# # losses for logits
-# # input shape: [B,C,H,W]
-# class ChannelWiseLoss(Baseloss):
-#     # 'Apply softmax channel wise'
-#     def __init__(self,weight,tau):
-#         super().__init__(weight,tau)
-#     def transform(self,x,gt_semantic_seg):
-#         x = self.resize(x,gt_semantic_seg)
-#         B,C,W,H = x.shape
-#         x = x.reshape(B,C,W*H)
-#         return x
-
-# class ChannelWiseLossWithMask(Baseloss):
-#     def __init__(self,weight,tau):
-#         super().__init__(weight,tau)
-#     def transform(self,x,gt_semantic_seg):
-#         x = self.resize(x,gt_semantic_seg)
-#         B,C,W,H = x.shape
-#         x = x.reshape(B,C,W*H)
-#         return x
-#     def forward(self,x_student,x_teacher,gt_semantic_seg):
-#         x_student,x_teacher = self.transform(x_student,gt_semantic_seg),self.transform(x_teacher,gt_semantic_seg)
-#         teacher_pd = torch.argmax(x_teacher,dim=1,keepdim=True)
-
-#         gt_semantic_seg = gt_semantic_seg.reshape(gt_semantic_seg.shape[0],1,-1)
-
-#         mask_tm = (teacher_pd != gt_semantic_seg).bool() # teacher mistakes
-#         mask_bg = (gt_semantic_seg == 255).bool() # background (255)
-#         mask = (mask_bg & mask_tm).expand(-1,150,-1)
-
-#         x_student[mask] = -1e9
-#         x_teacher[mask] = -1e9
-
-#         x_student = F.log_softmax(x_student/self.tau,dim=self.softmax_dim)
-#         x_teacher = F.softmax(x_teacher/self.tau,dim=self.softmax_dim)
-#         loss = self.weight*self.KLDiv(x_student,x_teacher)
-#         return loss/(x_student.numel()/x_student.shape[-1])
-
-
-# class SpatialWiseLoss(Baseloss):
-#     # 'Apply softmax spatial wise'
-#     def __init__(self,weight,tau):
-#         super().__init__(weight,tau)
-#     def transform(self,x,gt_semantic_seg):
-#         x = self.resize(x,gt_semantic_seg)
-#         B,C,W,H = x.shape
-#         x = x.permute(0,2,3,1)
-#         return x
-
-# class ChannelGroupLoss(Baseloss):
-#     # group the channels and apply softmax
-#     def __init__(self,weight,tau,group_num):
-#         super().__init__(weight,tau)
-#         self.group_num = group_num
-#     def transform(self,x,gt_semantic_seg):
-#         x = self.resize(x,gt_semantic_seg)
-#         B,C,W,H = x.shape
-#         x = x.reshape(B,C,W*H)
-#         x = x.reshape(B,C//self.group_num,self.group_num*W*H)
-#         return x
-
-# # class ChannelGroupLossWithMask(Baseloss):
-# #         # group the channels and apply softmax
-# #     def __init__(self,weight,tau,group_num):
-# #         super().__init__(weight,tau)
-# #         self.group_num = group_num
-# #     def transform(self,x,gt_semantic_seg):
-# #         x = self.resize(x,gt_semantic_seg)
-# #         B,C,W,H = x.shape
-# #         x = x.reshape(B,C,W*H)
-# #         x = x.reshape(B,C//self.group_num,self.group_num*W*H)
-# #         return x
-
-# class SpatialGroupLoss(Baseloss):
-#     # use a shift window to get spatial groups
-#     def __init__(self,weight,tau,kernel_size, dilation=1, padding=0, stride=1):
-#         super().__init__(weight,tau)
-#         self.kernel_size = kernel_size
-#         self.dilation = dilation
-#         self.padding = padding
-#         self.stride = stride
-#     def transform(self,x,gt_semantic_seg):
-#         x = self.resize(x,gt_semantic_seg)
-#         B,C,W,H = x.shape
-#         x = F.unfold(x,self.kernel_size, self.dilation, self.padding, self.stride)
-#         x = x.permute(0,2,1)
-#         return x
-
-# # losses for attention map
-# # input shape: [B,num_head,WH,256]
-
-# class ChannelWiseLossAttention(Baseloss):
-#     def __init__(self,weight,tau,softmax_dim=-1):
-#         super().__init__(weight,tau,softmax_dim = -1)
-#     def transform(self,x,gt_semantic_seg):
-#         B,num_head,WH,_ = x.shape
-#         x = x.mean(dim=1)
-#         x = x.permute(0,2,1)
-#         return x
-
-# class SpatialLossAttention(Baseloss):
-#     def __init__(self,weight,tau,softmax_dim=-1):
-#         super().__init__(weight,tau,softmax_dim = -1)
-#     def transform(self,x,gt_semantic_seg):
-#         x = x.mean(dim=1)
-#         return x
-
-# class ChannelGroupLossAttention(Baseloss):
-#     def __init__(self,weight,tau,group_num,softmax_dim = -1):
-#         super().__init__(weight,tau,softmax_dim = -1)
-#         self.group_num = group_num
-#     def transform(self,x,gt_semantic_seg):
-#         B,num_head,WH,_ = x.shape
-#         x = x.mean(dim=1)
-#         x = x.transpose(B,-1,WH)
-#         x = x.reshape(B,-1,self.group_num*WH)
-#         return x
-
-# # losses for feature
-# # input shape: [B,WH,C]
-# class ChannelWiseLossFeature(Baseloss):
-#     def __init__(self,weight,tau,softmax_dim=-1):
-#         super().__init__(weight,tau,softmax_dim = -1)
-#     def transform(self,x,gt_semantic_seg):
-#         x = x.permute(0,2,1)
-#         return x
-
-# class SpatialLossFeature(Baseloss):
-#     def __init__(self,weight,tau,softmax_dim=-1):
-#         super().__init__(weight,tau,softmax_dim = -1)
-#     def transform(self,x,gt_semantic_seg):
-#         # x = x.permute(0,2,1)
-#         return x
-# # ---------
+class FeedForward(nn.Module):
+    # feed forward apply to optional dim
+    def __init__(self,c_in,c_out,dim):
+        super().__init__()
+        self.ff = nn.Linear(c_in,c_out)
+        self.dim = dim
+    def forward(self,x):
+        x = x.transpose(1,self.dim)
+        x = self.ff(x)
+        x = x.transpose(1,self.dim)
+        return x
 
 class KLDLoss(nn.Module):
-    def __init__(self,weight,tau,softmax_dim):
+    def __init__(self,weight,tau,softmax_dim,ff=None):
         super().__init__()
         self.weight = weight
         self.tau = tau
         self.softmax_dim = softmax_dim
         self.KLDiv = torch.nn.KLDivLoss(reduction='sum')
+
+        if ff is None:
+            self.ff = None
+        else:
+            self.ff = FeedForward(**ff)
+
     def _resize(self,x,gt_semantic_seg):
         return x
     def _mask(self,x_student,x_teacher,gt_semantic_seg):
@@ -183,6 +38,8 @@ class KLDLoss(nn.Module):
     def _transform(self,x):
         return x
     def forward(self,x_student,x_teacher,gt_semantic_seg):
+        if self.ff is not None:
+            x_student = self.ff(x_student)
         x_student,x_teacher = self._resize(x_student,gt_semantic_seg),self._resize(x_teacher,gt_semantic_seg)
         x_student,x_teacher = self._mask(x_student,x_teacher,gt_semantic_seg)
         x_student,x_teacher = self._transform(x_student),self._transform(x_teacher)
@@ -195,16 +52,18 @@ class KLDLoss(nn.Module):
 
 # logits: [b,C,255,255]
 class ChannelGroupLossForLogits(KLDLoss):
-    def __init__(self,weight,tau,group_size,apply_mask=False):
-        super().__init__(weight,tau,softmax_dim=-1)
+    def __init__(self,weight,tau,group_size,apply_mask=False,apply_resize=True,ff=None):
+        super().__init__(weight,tau,softmax_dim=-1,ff=None)
         self.group_size = group_size
         self.apply_mask = apply_mask
+        self.apply_resize = apply_resize
     def _resize(self,x,gt_semantic_seg):
-        x= resize(
-            input=x,
-            size=gt_semantic_seg.shape[2:],
-            mode='bilinear',
-            align_corners=False)
+        if self.apply_resize:
+            x= resize(
+                input=x,
+                size=gt_semantic_seg.shape[2:],
+                mode='bilinear',
+                align_corners=False)
         return x
     def _mask(self,x_student,x_teacher,gt_semantic_seg):
         if not self.apply_mask:
@@ -226,8 +85,8 @@ class ChannelGroupLossForLogits(KLDLoss):
         return x
 
 class SpatialGroupLossForLogits(KLDLoss):
-    def __init__(self,weight,tau,kernel_size, dilation, padding, stride, apply_mask=False):
-        super().__init__(weight,tau,softmax_dim=-1)
+    def __init__(self,weight,tau,kernel_size, dilation, padding, stride, apply_mask=False,ff=None):
+        super().__init__(weight,tau,softmax_dim=-1,ff=None)
         self.kernel_size = kernel_size
         self.dilation = dilation
         self.padding = padding
@@ -260,8 +119,8 @@ class SpatialGroupLossForLogits(KLDLoss):
 
 # attention:[b,num_head,WH,WH']
 class ChannelGroupLossForAttention(KLDLoss):
-    def __init__(self,weight,tau,group_size):
-        super().__init__(weight,tau,softmax_dim=-1)
+    def __init__(self,weight,tau,group_size,ff=None):
+        super().__init__(weight,tau,softmax_dim=-1,ff=None)
         self.group_size = group_size
     def _transform(self,x):
         B,num_head,WH,WH_ = x.shape
@@ -271,8 +130,8 @@ class ChannelGroupLossForAttention(KLDLoss):
         return x
 
 class SpatialGroupLossForAttention(KLDLoss):
-    def __init__(self,weight,tau,kernel_size, dilation, padding, stride,):
-        super().__init__(weight,tau,softmax_dim=-1)
+    def __init__(self,weight,tau,kernel_size, dilation, padding, stride,ff=None):
+        super().__init__(weight,tau,softmax_dim=-1,ff=None)
         self.kernel_size = kernel_size
         self.dilation = dilation
         self.padding = padding
@@ -289,8 +148,8 @@ class SpatialGroupLossForAttention(KLDLoss):
 # feature:[B,WH,C]
 
 class ChannelGroupLossForFeature(KLDLoss):
-    def __init__(self,weight,tau,group_size):
-        super().__init__(weight,tau,softmax_dim=-1)
+    def __init__(self,weight,tau,group_size,ff=None):
+        super().__init__(weight,tau,softmax_dim=-1,ff=None)
         self.group_size = group_size
     def _transform(self,x):
         B,WH,C = x.shape
@@ -299,8 +158,8 @@ class ChannelGroupLossForFeature(KLDLoss):
         return x
 
 class SpatialGroupLossForFeature(KLDLoss):
-    def __init__(self,weight,tau,kernel_size, dilation, padding, stride):
-        super().__init__(weight,tau,softmax_dim=-1)
+    def __init__(self,weight,tau,kernel_size, dilation, padding, stride,ff=None):
+        super().__init__(weight,tau,softmax_dim=-1,ff=None)
         self.kernel_size = kernel_size
         self.dilation = dilation
         self.padding = padding
